1a32ed8d4916c114c58159b26849083fd2382b8a
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2025-07-15 02:26:21 +0900

    add mymap

---
+++ keyboards/crkbd/vial-kb/vial-qmk/keymaps/mymap/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x89, 0x36, 0x2A, 0xC7, 0xFA, 0xD8, 0x89, 0x45}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+// Define the number of layers
+#define LAYER_STATE_16BIT
---
+++ make.sh
@@ -0,0 +1,5 @@
+# !/bin/sh -v
+make vial-qmk-clean
+kb=crkbd make vial-qmk-init
+kb=crkbd kr=rev4_1/standard km=vial make vial-qmk-compile
+
